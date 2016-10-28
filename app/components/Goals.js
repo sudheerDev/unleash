5edc397ad59c84dda1b868759e2756b2fc520e7b
Kelvin De Moya <kdemoya17@gmail.com>
2016-10-27 23:54:35 -0400

    Updating Goals view to use GoalCard component

--- app/components/Goals.js
+++ app/components/Goals.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
-import List from 'material-ui/List/List';
-import ListItem from 'material-ui/List/ListItem';
+import GoalCard from './GoalCard';
+import _ from 'lodash';
+
+let styles = {};
 
 class Goals extends Component {
   componentDidMount() {
     this.props.actions.fetchGoals();
   }
 
+  renderGoals(goals) {
+    return _.map(goals, (goal) =>
+      <GoalCard key={goal.id} goal={goal} />
+    );
+  }
+
   render() {
     const { goals: { list } } = this.props;
     return (
-      <List>
-        {list.map(goal => (
-          <ListItem
-            key={goal.id}
-            primaryText={goal.name}
-          />
-        ))}
-      </List>
+      <div style={styles.goalsWrapper}>
+        {this.renderGoals(list)}
+      </div>
     );
   }
 }
@@ -25,6 +28,18 @@ class Goals extends Component {
 Goals.propTypes = {
   actions: React.PropTypes.object.isRequired,
   goals: React.PropTypes.object.isRequired,
+};
+
+styles = {
+  goalsWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    margin: 'auto',
+    width: '90%',
+    maxWidth: '1150px',
+  },
 };
 
 export default Goals;
