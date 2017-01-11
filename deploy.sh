f80c36c31f3fb93efbb6a1843f199ec8716bf4d1
Karol Sójko <karolsojko@gmail.com>
2017-01-11 15:00:55 +0100

    Add order of profiles on the list

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ set -ev
 TAG=$1
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker build -t xteam/unleash:$TAG .
-docker push xteam/unleash
+docker push xteam/unleash:$TAG
