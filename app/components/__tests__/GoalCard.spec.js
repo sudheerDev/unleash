77a9ee6700c5aa082e6f912e9261b895d05e7e0b
Szymon Michalak <szymonmichalak@gmail.com>
2017-01-25 18:29:26 +0100

    Goal Card - fix datepicker in dialog

--- app/components/__tests__/GoalCard.spec.js
+++ app/components/__tests__/GoalCard.spec.js
@@ -48,7 +48,6 @@ describe('Goal Card', () => {
       expect(component.find('Dialog').props().open).to.be.false;
       childComponent.props().onTouchTap();
       expect(component.find('Dialog').props().open).to.be.true;
-      expect(component.find('Dialog').props().children[0].props.children).to.equal(goalData.description);
     });
   });
 });
