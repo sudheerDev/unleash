47371aebd468efb63aac1ad3d4c4942e46c41229
Karol Sójko <karolsojko@gmail.com>
2017-03-30 13:59:41 +0200

    Creating skill resources with Serverless Skills API

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
  */
 
 module.exports = {
-  skills_api_url: 'http://skills-staging.unleash.x-team.com/api/v1/skills',
+  skills_api_url: 'https://fy5p532pni.execute-api.us-west-2.amazonaws.com/dev/skills',
   goals_api_url: 'http://goals-staging.unleash.x-team.com/api/v1/goals.json',
   profiles_api_url: 'https://txkaf3ohhf.execute-api.us-west-2.amazonaws.com/staging/profiles',
   paths_api_url: 'http://paths-staging.unleash.x-team.com/api/v1/paths',
